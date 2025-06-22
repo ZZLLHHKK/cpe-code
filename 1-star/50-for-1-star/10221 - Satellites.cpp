fb9aabd1f64083737b5785df265956d99bb4c123
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2025-06-22 17:57:15 +0800

    Create 10221 - Satellites.cpp

---
+++ 1-star/50-for-1-star/10221 - Satellites.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+const double pi = acos(-1);
+//1 min = 6 degree
+void solve(double s, double ang) {
+    double arc, ch;
+    arc = sqrt((2 * s * s) - (2 * s * s * cos(ang * pi / 180)));
+    ch = 2 * pi * s * (ang / 360.0);
+    cout << fixed << setprecision(6) << ch << " " << fixed << setprecision(6) << arc << '\n';
+}
+
+int main() {
+    int s, a;
+    string cmd;
+    
+    while (cin >> s >> a >> cmd) {
+        double arc, ch, ang;
+        s += 6440;
+        if (cmd == "deg") ang = (double)a;
+        else ang = a / 60.0;
+
+        if (ang == 180.0) {
+            solve(s, ang);
+            continue;
+        }
+        else if (ang == 360.0)
+            s = 0, ang = 0;
+            
+        while (ang >= 360.0) ang -= 180.0;
+        if (ang >= 180.0) ang = 360.0 - ang;
+        solve(s, ang);
+    }
+}
